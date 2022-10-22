b4707e9114398796579ebcceb93fbf027ccca175
iferas93 <ferasmo7mad@gmail.com>
2022-10-23 00:43:30 +0300

    config phpunit

---
+++ tests/QuranWrapperTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Iferas93\Mp3quranWrapper\Tests;
+
+class QuranWrapperTest extends TestCase
+{
+    public function test_its_work()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Iferas93\Mp3quranWrapper\Tests;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+
+}
