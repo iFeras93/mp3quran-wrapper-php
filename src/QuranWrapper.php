eb91eb56218fb731b19c3da26af77bf1b567a3bb
iferas93 <ferasmo7mad@gmail.com>
2022-10-23 00:55:05 +0300

    namespacing

--- src/QuranWrapper.php
+++ src/QuranWrapper.php
@@ -1,5 +1,7 @@
 <?php
+namespace Iferas93\Mp3quranWrapper;
 
+use GuzzleHttp\Client;
 
 class QuranWrapper
 {
@@ -7,15 +9,15 @@ class QuranWrapper
     private $baseUri="https://mp3quran.net/api";
     private $version ="v3";
 
-    public function serviceBaseUri()
+    protected function serviceBaseUri()
     {
         return $this->baseUri.'/'.$this->version;
     }
 
     #TODO:: move this business logic to another class and do refactor
-    public function makeRequest($endpoint,$method = 'GET',$queryParams=[],$bodyParams=[],$headers=[])
+    protected function makeRequest($endpoint,$method = 'GET',$queryParams=[],$bodyParams=[],$headers=[])
     {
-        $client = new \GuzzleHttp\Client([
+        $client = new Client([
             // Base URI is used with relative requests
             'base_uri' => $this->serviceBaseUri(),
             // You can set any number of default request options.
@@ -35,6 +37,6 @@ class QuranWrapper
 
     public function getAvailableContentLanguages()
     {
-
+        return $this->makeRequest('languages');
     }
 }
